a87cf56bb0c6096fa1687d6ed03476e150f888c0
Alessandro Rodríguez <alessrodrgz@gmail.com>
2022-12-26 04:10:13 +0100

    changed to auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,10 +6,7 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter({
-      edge: false,
-      external: []
-    })
+    adapter: adapter()
   }
 }
 
